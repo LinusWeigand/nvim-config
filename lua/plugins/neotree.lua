56404a02758254c22e966911dc215e5ce608d0a0
Linus Weigand <linusweigand@protonmail.com>
2024-03-06 14:06:28 +0100

    add astro icons to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,48 +1,71 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
-		vim.keymap.set("n", "<leader>o", ":Neotree toggle<CR>", {})
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
+    vim.keymap.set("n", "<leader>o", ":Neotree toggle<CR>", {})
 
-		require("neo-tree").setup({
-			close_if_last_window = true, -- Close Neo-tree if it's the last window
-			filesystem = {
-				follow_current_file = true, -- Automatically navigate to the current file in the tree
-				use_libuv_file_watcher = true, -- Use faster file watcher based on libuv
-				enable_git_status = true,
-				git_status_async = true,
-			},
-			window = {
-				mappings = {
-					["l"] = "open", -- Additional mapping for 'l' to open files and directories
-					["h"] = "close_node", -- Additional mapping for 'l' to open files and directories
-				},
-			},
-			git_status = {
-				window = {
-					mappings = {
-						["l"] = "open", -- Additional mapping for 'l' in Git status
-					},
-				},
-			},
-			source_selector = {
-				winbar = true,
-				content_layout = "center",
-				sources = {
-					"filesystem",
-					"buffers",
-					"git_status",
-					"diagnostics",
-				},
-			},
-		})
-	end,
+    require("neo-tree").setup({
+      auto_clean_after_session_restore = true,
+      close_if_last_window = true,
+      filesystem = {
+        follow_current_file = true,
+        use_libuv_file_watcher = true,
+        enable_git_status = true,
+        git_status_async = true,
+      },
+      window = {
+        mappings = {
+          ["l"] = "open",  -- Additional mapping for 'l' to open files and directories
+          ["h"] = "close_node", -- Additional mapping for 'l' to open files and directories
+        },
+      },
+      git_status = {
+        window = {
+          mappings = {
+            ["l"] = "open", -- Additional mapping for 'l' in Git status
+          },
+        },
+      },
+      source_selector = {
+        winbar = true,
+        content_layout = "center",
+        sources = {
+          { source = "filesystem", display_name = "" .. " File" },
+          { source = "buffers", display_name = "" .. " Bufs" },
+          { source = "git_status", display_name = "" .. " Git" },
+          { source = "diagnostics", display_name = "裂" .. " Diagnostic" },
+
+          default_component_configs = {
+            indent = { padding = 0, indent_size = 1 },
+            icon = {
+              folder_closed = "",
+              folder_open = "",
+              folder_empty = "",
+              default = "",
+            },
+            modified = { symbol = "" },
+            git_status = {
+              symbols = {
+                added = "",
+                deleted = "",
+                modified = "",
+                renamed = "➜",
+                untracked = "★",
+                ignored = "◌",
+                unstaged = "✗",
+                staged = "✓",
+                conflict = "",
+              },
+            },
+          },
+        },
+      },
+    })
+  end,
 }
-
--- lua/set.lua
+++ lua/set.lua
@@ -35,8 +35,10 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
+
 -- Comment
 vim.keymap.set('n', '<leader>/', function()
 	require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
 	end, {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap('v', '<leader>/', "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", {noremap = true, silent = true})
+
