7782697f18eff6a4b99f923f49237652c1f0ae07
Linus Weigand <linusweigand@protonmail.com>
2024-08-02 13:20:35 +0200

    switch to nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,18 @@
 return {
-  -- "nvim-tree/nvim-tree.lua",
-  -- version = "*",
-  -- lazy = false,
-  -- dependencies = {
-  --   "nvim-tree/nvim-web-devicons",
-  -- },
-  -- config = function()
-  --   require("nvim-tree").setup {}
-  -- end,
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+      view = {
+        side = "left",
+        adaptive_size = true
+      }
+
+    }
+
+  end,
 }
