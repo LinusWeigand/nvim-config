8643f9be7bb9be425cac87471c236449a6c538b6
Linus Weigand <linusweigand@protonmail.com>
2024-08-02 13:20:44 +0200

    experiment with oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  -- 'stevearc/oil.nvim',
+  -- opts = {},
+  -- dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- config = function()
+  --   local oil = require("oil");
+  --   oil.setup({
+  --     default_file_explorer = true,
+  --
+  --     colums = {
+  --       "icon",
+  --     }
+  --   })
+  -- end
+}
