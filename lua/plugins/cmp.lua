f3e606c8b76035d636681ee51ef9b168eb8879ab
Linus Weigand <linusweigand@protonmailcom>
2024-10-23 11:42:36 +0200

    remove overlaying def autocmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,38 +1,38 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp"
-  },
-  -- {
-  --   "github/copilot.vim"
-  -- },
-  {
-    "windwp/nvim-autopairs", -- Add this plugin
-    config = function()
-      require("nvim-autopairs").setup({})
-    end
-  },
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	-- {
+	--   "github/copilot.vim"
+	-- },
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets"
-		}
+			"rafamadriz/friendly-snippets",
+		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+						require("luasnip").lsp_expand(args.body)
 					end,
 				},
 				window = {
 					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -47,6 +47,16 @@ return {
 				}, {
 					{ name = "buffer" },
 				}),
+				completion = {
+					keyword_length = 2, -- Start suggesting after 2 characters
+					completeopt = "menu,menuone,noselect", -- Customize completion behavior
+				},
+				-- Customize matching strategy for better filtering
+				matching = {
+					disallow_fuzzy_matching = true, -- Disable fuzzy matching
+					disallow_partial_matching = false, -- Allow partial matching
+					disallow_prefix_unmatching = true, -- Disable unmatching prefixes
+				},
 			})
 		end,
 	},
